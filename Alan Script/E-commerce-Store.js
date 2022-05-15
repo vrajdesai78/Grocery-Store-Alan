be257ec92a38957a17e93bb5ce5b7745ca9232a0
vrajdesai78 <vrajdesai78@gmail.com>
2022-05-15 17:39:46 +0530

    Alan Script added

---
+++ Alan Script/E-commerce-Store.js
@@ -0,0 +1,54 @@
+onCreateUser(p => {
+    p.userData.itemList = {en:"fresh banana_|red apple_|ginger|rice|diet coke|coca cola can_|sprite can_|apple and grape juice|orange juice|pepsi can_"};
+})
+
+intent("What is this app", p=> {
+    p.play("This is an online e-commerce app where you can purchase any groceries online")
+});
+
+intent("(Show|Open) me my (favourite|favorite) items", p=> {
+    p.play({command: "showFavorite"});
+    p.play("Showing you your favorite items");
+});
+
+intent("(Show|Open) my cart items", p=> {
+    p.play({command: "showCart"});
+    p.play("Showing items present in your cart")
+})
+
+intent("(Go|) (back to|) home", p=> {
+    p.play({command: "showHome"});
+    p.play("Navigating back to Home")
+})
+
+intent("(Go|Show|Go to) my profile", p=> {
+    p.play({command: "showProfile"});
+    p.play("Showing you your profile")
+})
+
+intent(`(Add|I want|want) $(ITEM u:itemList) to favorite`, p=> {
+    p.play({command: "addFavorite", item: `${p.ITEM.value}`});
+    p.play(`${p.ITEM.value} is added to your favorites`)
+})
+
+intent(`(Add|I want|want) $(NUMBER) (kg|gram|grams|) $(ITEM u:itemList) (to cart|)`, p => {
+     var a = "";
+     if(p.ITEM.value == "ginger" || p.ITEM.value == "rice") {
+         if(p.NUMBER.value >= 250 && p.NUMBER.value%250 == 0) {
+             a = `${p.NUMBER.value} gram`;
+             p.NUMBER.value = p.NUMBER.value/250;
+         }
+         else if(p.NUMBER.value >= 0 && p.NUMBER.value <= 100) {
+             a = `${p.NUMBER.value} kg`;
+             p.NUMBER.value *= 4;
+         }
+     }
+    else if(p.ITEM.value == "red apple_" || p.ITEM.value == "fresh banana_"){
+        a = `${p.NUMBER.value} kg`;
+    }
+    else {
+        a = `${p.NUMBER.value}`;
+    }
+     p.play({command: "addItem", item: `${p.ITEM.value}`, count: `${p.NUMBER.value}`});
+     p.play(`Done. Added ${a} ${p.ITEM.value}`);
+});
